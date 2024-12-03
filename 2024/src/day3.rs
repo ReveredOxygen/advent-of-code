1d9cb222c9eda14faf2a21c5f159858db2375854
ReveredOxygen <reveredoxygen@gmail.com>
2024-12-03 18:52:11 -0500

    Replace regex by hand written parser

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -1,44 +1,143 @@
 use aoc_runner_derive::aoc;
-use lazy_regex::regex;
+use std::str;
 
 #[aoc(day3, part1)]
-fn part1(input: &str) -> i64 {
-    let re = regex!(r#"mul\((?<d1>\d+),(?<d2>\d+)\)"#);
-
+fn part1(mut input: &str) -> u64 {
     let mut acc = 0;
 
-    for cap in re.captures_iter(input) {
-        acc += cap["d1"].parse::<i64>().unwrap() * cap["d2"].parse::<i64>().unwrap();
-    }
-
-    acc
-}
-
-#[aoc(day3, part2)]
-fn part2(input: &str) -> i64 {
-    let re = regex!(r#"(?<tog>do(n't)?\(\))|(mul\((?<d1>\d+),(?<d2>\d+)\))"#);
-
-    let mut acc = 0;
-    let mut mul = true;
-
-    for cap in re.captures_iter(input) {
-        if let Some(s) = cap.name("tog") {
-            if s.len() == 4 {
-                mul = true;
-            } else {
-                mul = false;
+    while input.len() >= 8 {
+        match parse_mul(input) {
+            Some((res, consumed)) => {
+                acc += res;
+                input = &input[consumed..];
             }
-        } else if mul {
-            acc += cap["d1"].parse::<i64>().unwrap() * cap["d2"].parse::<i64>().unwrap();
+            None => input = &input[1..],
         }
     }
 
     acc
 }
 
+#[aoc(day3, part2)]
+fn part2(mut input: &str) -> u64 {
+    let mut acc = 0;
+
+    let mut parsing = true;
+
+    while input.len() >= 8 {
+        if let Some((continuing, consumed)) = parse_do_dont(input) {
+            parsing = continuing;
+            input = &input[consumed..];
+        } else if parsing {
+            match parse_mul(input) {
+                Some((res, consumed)) => {
+                    acc += res;
+                    input = &input[consumed..];
+                }
+                None => input = &input[1..],
+            }
+        } else {
+            input = &input[1..];
+        }
+    }
+
+    acc
+}
+
+fn parse_do_dont(input: &str) -> Option<(bool, usize)> {
+    if !input.as_bytes().starts_with(b"do") {
+        return None;
+    }
+
+    if &input.as_bytes()[2..4] == b"()" {
+        return Some((true, 4));
+    } else if &input.as_bytes()[2..7] == b"n't()" {
+        return Some((false, 7));
+    }
+
+    return None;
+}
+
+fn parse_mul(input: &str) -> Option<(u64, usize)> {
+    if !input.as_bytes().starts_with(b"mul(") {
+        return None;
+    }
+
+    let mut consumed = 4;
+
+    let mut args = input[4..].as_bytes();
+
+    let mut n1: u64 = 0;
+    let mut n2: u64 = 0;
+
+    let mut broken = false;
+
+    for i in 0..3 {
+        if b'0' <= args[i] && args[i] <= b'9' {
+            n1 *= 10;
+            n1 += (args[i] - b'0') as u64;
+        } else if args[i] == b',' {
+            if i > 0 {
+                args = &args[i + 1..];
+                consumed += i + 1;
+                broken = true;
+                break;
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
+    }
+
+    if !broken && args[3] != b',' {
+        return None;
+    } else if !broken {
+        args = &args[4..];
+        consumed += 4;
+    }
+
+    for i in 0..4 {
+        if b'0' <= args[i] && args[i] <= b'9' {
+            n2 *= 10;
+            n2 += (args[i] - b'0') as u64;
+        } else if args[i] == b')' {
+            if i > 0 {
+                return Some((n1 * n2, consumed + i + 1));
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
+    }
+
+    unreachable!()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn parse_do() {
+        assert_eq!(parse_do_dont("do()"), Some((true, 4)));
+    }
+
+    #[test]
+    fn parse_dont() {
+        assert_eq!(parse_do_dont("don't()"), Some((false, 7)));
+    }
+
+    #[test]
+    fn mul_parser() {
+        assert_eq!(parse_mul("mul(2,412)fhgweuiosfh"), Some((824, 10)));
+    }
+
+    #[test]
+    fn mul_parser2() {
+        assert_eq!(parse_mul("mul(177,274)when"), Some((48498, 12)));
+    }
 
     #[test]
     fn part1_example() {
